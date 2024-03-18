925b6e21ed576664dccf2f144454b94ccea32336
mothishkumar.k <mothishkumar11@gmail.com>
2024-03-19 02:17:24 +0530

    changed transition

--- src/components/home/product.jsx
+++ src/components/home/product.jsx
@@ -68,8 +68,8 @@ const Product = () => {
       opacity: 1,
       scale: 1,
       transition: {
-        delayChildren: 0.3,
-        staggerChildren: 0.2
+        delayChildren: 0.2,
+        staggerChildren: 0.1
       }
     }
   };
